a7801eb5885d5e554c01cc9b41228bef88fa121f
Danny Roubin <dannyroubin1@gmail.com>
2021-07-15 16:40:11 -0700

    created the first portion of the schema, adding in seed data onto the schema file

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -5,15 +5,29 @@ use employeeTrackerDB
 
 -- add department name and id here
 create table department(
+id int not null auto_increment
+name varchar(30)
+primary key (id)
 
 )
 
 -- add title, salary, and department id
 create table 'role'(
-
+id int not null auto_increment
+title varchar(30)
+salery decimal (10,2)
+department_id int not null
+primary key (id)
 )
 
 -- add first and last name, role id, and manager id
 create table employee(
-
+id int not null auto_increment
+first_name varchar(30) not null
+last_name varchar(30) not null
+role_id int not null
+manager_id int
+primary key (id)
 )
+
+
