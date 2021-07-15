01a3473e708bcccb8c07252b8e6bbc97a25205c6
Danny Roubin <dannyroubin1@gmail.com>
2021-07-15 15:11:13 -0700

    began creating the schema

---
+++ employeeTrackerSchema.sql
@@ -0,0 +1,19 @@
+-- creates the database employeeTrackerDB and sets it as the one in use
+drop database if exists employeeTrackerDB
+create database employeeTrackerDB
+use employeeTrackerDB
+
+-- add department name and id here
+create table department(
+
+)
+
+-- add title, salary, and department id
+create table 'role'(
+
+)
+
+-- add first and last name, role id, and manager id
+create table employee(
+
+)
