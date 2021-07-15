5b66ee99e3845e0e3037b1eed1b1a071f07d5519
Danny Roubin <dannyroubin1@gmail.com>
2021-07-15 16:58:28 -0700

    added in seed data onto the schema folder

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -30,4 +30,35 @@ manager_id int
 primary key (id)
 )
 
+-- adding new info to tables
 
+-- creates a new dapartment titled software engineers
+insert into department (name)
+value ("Software Engineers")
+
+-- creating the following four roles
+insert into 'role' (title, salary, department_id)
+values (Front-end developer, 90,000, 19)
+
+insert into 'role' (title, salary, department_id)
+values (Back-end developer, 92,000, 11)
+
+insert into 'role' (title, salary, department_id)
+values (project manager, 120,000, 16)
+
+insert into 'role' (title, salary, department_id)
+values (front-end designer, 88,000, 12)
+
+-- creates new employees
+
+insert into employee (first_name, last_name, role_id, manager_id)
+values ("Bill", "Rockington", 19, 5)
+
+insert into employee (first_name, last_name, role_id, manager_id)
+values ("Jill", "Doe", 16, 5)
+
+insert into employee (first_name, last_name, role_id, manager_id)
+values ("Garrett", "Grant", 11, 5)
+
+insert into employee (first_name, last_name, role_id, manager_id)
+values ("Vincent", "Burns", 12, 5)
