b40d875f8ec70d12272d7bfea772c14ab1fbe061
Danny Roubin <dannyroubin1@gmail.com>
2021-07-17 20:38:46 -0700

    added the start of the inquirer prompt and half of the switch cases

--- index.js
+++ index.js
@@ -3,15 +3,76 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 const dotenvConnection = mysql.createConnection({
-    host: "localhost", 
-    port: 3301,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+  host: "localhost",
+  port: 3301,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
+dotenvConnection.connect((err) => {
+  if (err) throw err;
+  startTask();
+});
 
+const startTask = () => {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "View All Roles",
+        "Add Role",
+        "Remove Role",
+        "View All Departments",
+        "Add Department",
+        "Remove Department",
+        "View Total Budget By Department",
+        "Exit",
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case "View All Employees":
+          artistSearch();
+          break;
 
-// Refer to week 12 activity 14 for how to set this all up
+        case "View All Employees By Department":
+          multiSearch();
+          break;
 
-//refer to week 13 activity 16 to see how to get the .env up and running
+        case "View All Employees By Manager":
+          rangeSearch();
+          break;
+
+        case "Add Employee":
+          songSearch();
+          break;
+
+        case "Remove Employee":
+          songSearch();
+          break;
+
+        case "Update Employee":
+          songSearch();
+          break;
+
+        case "Update Employee Role":
+          songSearch();
+          break;
+
+        case "Update Employee Manager":
+          songSearch();
+          break;
+      }
+    });
+};
