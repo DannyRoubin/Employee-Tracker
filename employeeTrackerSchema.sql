444986b9d413a5796d8d205673177910e9626f7f
Danny Roubin <dannyroubin1@gmail.com>
2021-07-18 13:42:47 -0700

    fixed up the schema to connect managers now

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -14,10 +14,11 @@ primary key (id)
 -- add title, salary, and department id
 create table `role` (
 id int not null auto_increment,
-title varchar(90),
+title varchar(30),
 salary decimal (10,2),
 department_id int not null,
-primary key (id)
+primary key (id),
+foreign key fkey_department (department_id) references department(id)
 );
 
 -- add first and last name, role id, and manager id
@@ -27,7 +28,9 @@ first_name varchar(30) not null,
 last_name varchar(30) not null,
 role_id int not null,
 manager_id int,
-primary key (id)
+primary key (id),
+foreign key fkey_manager (manager_id) references employee(id),
+foreign key fkey_role (role_id) references `role`(id)
 );
 
 
@@ -41,27 +44,39 @@ value ("Software Engineers");
 
 -- creating the following four roles
 insert into `role` (title, salary, department_id)
-values ('Front end developer', 90000, 19);
+values ('Front end developer', 90000, 1);
 
 insert into `role` (title, salary, department_id)
-values ('Back end developer', 92000, 11);
+values ('Back end developer', 92000, 1);
 
 insert into `role` (title, salary, department_id)
-values ('project manager', 120000, 16);
+values ('project manager', 120000, 1);
 
 insert into `role` (title, salary, department_id)
-values ('front end designer', 88000, 12);
+values ('front end designer', 88000, 1);
 
 -- creates new employees
 
 insert into employee (first_name, last_name, role_id, manager_id)
-values ("Bill", "Rockington", 19, 5);
+values ("Bill", "Rockington", 3, 1 );
 
 insert into employee (first_name, last_name, role_id, manager_id)
-values ("Jill", "Doe", 16, 5);
+values ("Jill", "Doe", 2, 1);
 
 insert into employee (first_name, last_name, role_id, manager_id)
-values ("Garrett", "Grant", 11, 5);
+values ("Garrett", "Grant", 1, 1);
 
 insert into employee (first_name, last_name, role_id, manager_id)
-values ("Vincent", "Burns", 12, 5);
+values ("Vincent", "Burns", 4, 1);
+
+ select 
+   employee.*, 
+   employeeManager.first_name as manager_first, 
+   employeeManager.last_name as manager_last,
+   `role`.title as role_title,
+   `role`.salary as role_salary,
+   department.name as department_name
+ from employee
+ inner join employee as employeeManager on employee.manager_id = employeeManager.id
+ inner join `role` on employee.role_id = `role`.id
+ inner join department on `role`.department_id = department.id
